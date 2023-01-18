7297281ce4c67fd93ed85c9ff81dea8ec6772c6e
gustavossh <106892137+gustavossh@users.noreply.github.com>
2023-01-18 12:10:52 -0300

    Add files via upload

--- bot.php
+++ bot.php
@@ -1,6 +1,6 @@
 <?php
 
-// Bot criado por @DARKNETSSH código original para o @JSINFINITY
+// Bot criado por @DARKNETSSH código original para o @DARKNETSSHBOT
 
 date_default_timezone_set ('America/Sao_Paulo'); // define timestamp padrão
 
@@ -56,7 +56,7 @@ switch ($tlg->Text ()){
 
 	$tlg->sendMessage ([
 		'chat_id' => $tlg->ChatID (),
-		'text' => 'Bot E versão1.0 sempre vai melhorar'
+		'text' => 'Bot original @NITYSSH por @NITYSSH'
 	]);
 
 	break;
@@ -90,7 +90,7 @@ switch ($tlg->Text ()){
 
 		exec ('./gerarusuario.sh '.$usuario.' '.$senha.' 1 1');
 
-		$textoSSH="🇧🇷 Conta SSH criada ;)\r\n\r\n<b>Servidor:</b> <code>".$ip."</code>\r\n<b>Usuario:</b> <code>".$usuario."</code>\r\n<b>Senha:</b> <code>".$senha."</code>\r\n<b>Logins:</b> 1\r\n<b>Validade:</b> ".date ('d/m', strtotime('+1 day'))."\r\n\r\n🤙 Cortesia do @JSINFINITY";
+		$textoSSH="🇧🇷 Conta SSH criada ;)\r\n\r\n<b>Servidor:</b> <code>".$ip."</code>\r\n<b>Usuario:</b> <code>".$usuario."</code>\r\n<b>Senha:</b> <code>".$senha."</code>\r\n<b>Logins:</b> 1\r\n<b>Validade:</b> ".date ('d/m', strtotime('+1 day'))."\r\n\r\n🤙 Cortesia do @NITYSSH";
 
 		$redis->setex ($tlg->UserID (), 43200, 'true'); //define registro para ser guardado por 12h
 
